f58ea1414588c3921fca36b565a3c307398a9d73
seoyulpad <hcom0103@gmail.com>
2017-05-26 01:28:38 +0900

    sing_up 액티비티 생성 - 회원가입으로 사용할거임

---
+++ app/src/main/java/com/example/chaes/projectlittlecare/LCClass/SignUp.java
@@ -0,0 +1,15 @@
+package com.example.chaes.projectlittlecare.LCClass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.chaes.projectlittlecare.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
