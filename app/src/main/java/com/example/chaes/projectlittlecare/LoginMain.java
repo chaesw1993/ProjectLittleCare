dc09c1c4abede7ca53a40c1b1e3a7563ab3f05bb
재형 <jaehyoung944@gmail.com>
2017-05-29 02:28:58 +0900

    login화면 아이디 비번 찾기 추가

--- app/src/main/java/com/example/chaes/projectlittlecare/LoginMain.java
+++ app/src/main/java/com/example/chaes/projectlittlecare/LoginMain.java
@@ -1,12 +1,13 @@
 package com.example.chaes.projectlittlecare;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.chaes.projectlittlecare.LCClass.FindInfo;
 import com.example.chaes.projectlittlecare.LCClass.SignUp;
 import com.example.chaes.projectlittlecare.LCClass.UnPFlag;
 
@@ -23,6 +24,7 @@ public class LoginMain extends AppCompatActivity {
         Button btn = (Button) findViewById(R.id.btn_login);
         Button btn2 = (Button) findViewById(R.id.btn_login2);
         TextView textView = (TextView) findViewById(R.id.link_signup);
+        TextView textView2 = (TextView) findViewById(R.id.link_find);
 
         btn.setOnClickListener(new View.OnClickListener() { // 제공자
             @Override
@@ -50,6 +52,15 @@ public class LoginMain extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginMain.this, SignUp.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginMain.this, FindInfo.class);
                 startActivity(intent);
                 //finish();
             }
