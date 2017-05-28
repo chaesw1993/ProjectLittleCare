04bbda083f3baab3ddfb5536d3fa27bce0190078
chaes <chaesw1993@gmail.com>
2017-05-29 04:27:57 +0900

    Pull 하기 위해 Commit // 오류부분 주석처리

--- app/src/main/java/com/example/chaes/projectlittlecare/Fragment_center_matching.java
+++ app/src/main/java/com/example/chaes/projectlittlecare/Fragment_center_matching.java
@@ -21,7 +21,7 @@ public class Fragment_center_matching extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_center_matching, container, false);
-
+/*
         ListView center_list;
         CenterMatchingAdapter adapter;
 
@@ -43,7 +43,7 @@ public class Fragment_center_matching extends Fragment {
                 int age = Integer.parseInt(item.getAge());
             }
         });
-
+*/
         return view;
     }
 }
