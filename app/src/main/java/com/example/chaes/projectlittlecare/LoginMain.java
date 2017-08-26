690103e8f9e7f66b4844a822f27b058e28ded496
Seoyul YOON <seoyul1993@gmail.com>
2017-08-27 05:40:19 +0900

    Merge branch 'master' of https://github.com/chaesw1993/ProjectLittleCare
    # Conflicts:
    #	app/src/main/java/com/example/chaes/projectlittlecare/LoginMain.java

--- app/src/main/java/com/example/chaes/projectlittlecare/LoginMain.java
+++ app/src/main/java/com/example/chaes/projectlittlecare/LoginMain.java
@@ -2,13 +2,15 @@ package com.example.chaes.projectlittlecare;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,10 +21,14 @@ import com.android.volley.toolbox.Volley;
 import com.example.chaes.projectlittlecare.LCClass.FindInfo;
 import com.example.chaes.projectlittlecare.LCClass.SignUp;
 import com.example.chaes.projectlittlecare.LCClass.UnPFlag;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,7 +39,14 @@ public class LoginMain extends AppCompatActivity {
     private RequestQueue requestQueue;
     private StringRequest request;
     private EditText email, password;
-    private Button btn, btn2;
+    private Button btn;
+    String TAG = "MainActivity";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    DatabaseReference myRef;
+    FirebaseUser fbUser;
+    ProgressBar pbLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +55,11 @@ public class LoginMain extends AppCompatActivity {
 
 
         btn = (Button) findViewById(R.id.btn_login);
-        btn2 = (Button) findViewById(R.id.btn_login2);
         TextView textView = (TextView) findViewById(R.id.link_signup);
         TextView textView2 = (TextView) findViewById(R.id.link_find);
         email = (EditText)findViewById(R.id.input_email);
         password = (EditText)findViewById(R.id.input_password);
+        pbLogin = (ProgressBar)findViewById(R.id.pbLogin);
 
         requestQueue = Volley.newRequestQueue(this);
 
@@ -62,7 +75,7 @@ public class LoginMain extends AppCompatActivity {
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener() { // 유저
+        btn.setOnClickListener(new View.OnClickListener() { // 유저
             @Override
             public void onClick(View v) {
 
@@ -104,6 +117,12 @@ public class LoginMain extends AppCompatActivity {
 
                 requestQueue.add(request);
 
+                if (email.getText().toString().isEmpty() ||email.getText().toString().equals("") ||password.getText().toString().isEmpty() || password.getText().toString().equals("") ){
+                    Toast.makeText(LoginMain.this, "잘못입력하셨습니다.", Toast.LENGTH_SHORT).show();
+                } else {
+                    userLogin(email.getText().toString(), password.getText().toString());
+                }
+
             }
         });
 
@@ -126,5 +145,40 @@ public class LoginMain extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    private void userLogin(String email, String password){
+        pbLogin.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        pbLogin.setVisibility(View.GONE);
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginMain.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+
+                        }
+                    }
+                });
     }
 }
